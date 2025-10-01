8c8fbf3e3a7e2e364e07337cce9bc1ce4039ff87
Smartbus-360 <smartbus365@gmail.com>
2025-10-01 14:01:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: "127.0.0.1",
-  user: "gpsuser",
+  user: "000",
   password: "Gps@2025Secure!",
   database: "tracking_db"
 });
