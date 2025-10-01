5b69d3cbe89bc99f745ffb4284743fef178bf6b0
Smartbus-360 <smartbus365@gmail.com>
2025-10-01 14:18:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ import stopRoutes from "./routes/stop.js";
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/api", authRoutes);
 app.use("/api/schools", schoolRoutes);
